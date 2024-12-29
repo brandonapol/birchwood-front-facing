6734d4c174bfdd9337790b96ad966be78a91270a
Brandon Apol <brandonapol@cedarville.edu>
2024-12-28 21:27:25 -0500

    feat: add tantanmen ramen

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,7 @@ import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
 
-// const googleSheetsData = async () => {
-//   const headers = {
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': '*',
-//   }
-//   const response = await fetch('http://localhost:5242/sheets', { headers })
-//   const data = await response.json()
-//   console.log(data)
-// }
+
 
 </script>
 
